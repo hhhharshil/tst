dd74a092b3e1744ffb6fcd53830851361e2830f4
hhhharshil <73005050+hhhharshil@users.noreply.github.com>
2023-09-13 16:51:40 -0400

    Create hello_world.sh

---
+++ hello_world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Usage: Hello World Bash Shell Script Using Variables
+# -------------------------------------------------
+ 
+# Define bash shell variable called var 
+# Avoid spaces around the assignment operator (=)
+var="Hello World"
+ 
+# print it 
+echo "$var"
